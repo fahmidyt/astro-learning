5386bd3d611cec8f9ca267805320dd0025e9e887
fmidyt <hxerfahmi@gmail.com>
2024-06-18 08:03:14 +0700

    feat: adaptor of netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,13 @@ import icon from "astro-icon";
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 
+import netlify from '@astrojs/netlify'
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), tailwind(), icon()],
+  output: 'hybrid',
+  adapter: netlify(),
   image: {
     service: {
       entrypoint: "astro/assets/services/sharp"
